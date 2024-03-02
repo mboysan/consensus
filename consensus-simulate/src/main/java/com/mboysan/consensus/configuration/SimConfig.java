a5b6d5cf45cfd805535ee8b8f0fe204cb7d91cbb
mboysan <dev@mboysan.com>
2024-03-02 11:10:12 +0200

    THESIS-T-97: SimConfig Params

--- consensus-simulate/src/main/java/com/mboysan/consensus/configuration/SimConfig.java
+++ consensus-simulate/src/main/java/com/mboysan/consensus/configuration/SimConfig.java
@@ -2,15 +2,21 @@ package com.mboysan.consensus.configuration;
 
 public interface SimConfig extends NodeConfig {
 
-    @Key("simulate.leaderId")
+    interface Param {
+        String LEADER_ID = "simulate.leaderId";
+        String FORWARD_TO_LEADER = "simulate.follower.forwardToLeader";
+        String BROADCAST_TO_FOLLOWERS = "simulate.leader.broadcastToFollowers";
+    }
+
+    @Key(Param.LEADER_ID)
     @DefaultValue("0")
     int leaderId();
 
-    @Key("simulate.follower.forwardToLeader")
+    @Key(Param.FORWARD_TO_LEADER)
     @DefaultValue("true")
     boolean forwardToLeader();
 
-    @Key("simulate.leader.broadcastToFollowers")
+    @Key(Param.BROADCAST_TO_FOLLOWERS)
     @DefaultValue("true")
     boolean broadcastToFollowers();
 
